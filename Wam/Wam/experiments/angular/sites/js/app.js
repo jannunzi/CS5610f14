6c308aff51502bf0446858b97f2cd8d0187f694d
jannunzi <jannunzi@gmail.com>
2014-04-11 09:38:33 -0400

    Angular Routing Sites

---
+++ Wam/Wam/experiments/angular/sites/js/app.js
@@ -0,0 +1,60 @@
+﻿
+var app = angular.module("sites", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+    when("/", {
+        templateUrl: "partials/sites.html",
+        controller: "SitesController"
+    }).
+    when("/sites", {
+        templateUrl: "partials/sites.html",
+        controller: "SitesController"
+    }).
+    when("/sites/:siteId/towers", {
+        templateUrl: "partials/towers.html",
+        controller: "TowersController"
+    }).
+    when("/sites/:siteId/towers/:towerId/equipments", {
+        templateUrl: "partials/equipments.html",
+        controller: "EquipmentsController"
+    }).
+    otherwise({
+        redirectTo: "/"
+    })
+}]);
+
+app.controller("SitesController", function ($scope) {
+    $scope.addSite = function () {
+        var newSite = {
+            id: $scope.newSiteName,
+            name: $scope.newSiteName,
+            description: $scope.newSiteDescription
+        };
+        $scope.sites[$scope.newSiteName] = newSite;
+    };
+    $scope.deleteSite = function (site) {
+        app.deleteSite($scope.sites, site);
+    };
+    $scope.sites = sites;
+});
+
+app.controller("TowersController", function ($scope, $routeParams) {
+    $scope.deleteSite = function (site) {
+        app.deleteSite($scope.sites, site);
+    }
+    $scope.sites = sites;
+    $scope.site = sites[$routeParams.siteId];
+    $scope.towers = $scope.site.towers;
+});
+
+app.controller("EquipmentsController", function ($scope, $routeParams) {
+    $scope.site = sites[$routeParams.siteId];
+    $scope.towers = $scope.site.towers;
+    $scope.tower = $scope.towers[$routeParams.towerId];
+    $scope.equipments = $scope.tower.equipments;
+});
+
+app.deleteSite = function(sites, site) {
+    delete sites[site.id];
+}
---
+++ Wam/Wam/experiments/angular/sites/js/data.js
@@ -0,0 +1,92 @@
+﻿var sites = {
+    site1: {
+        id: "site1",
+        name: "Site 1",
+        description: "Description 1",
+        towers: {
+            tower1: {
+                id: "tower1",
+                name: "Tower 1",
+                height: 123,
+                equipments: {
+                    equipment1: {
+                        id: "equipment1",
+                        name: "Equipment 1",
+                        brand: "ABB",
+                        price: 321
+                    },
+                    equipment2: {
+                        id: "equipment2",
+                        name: "Equipment 2",
+                        brand: "Acme",
+                        price: 432
+                    }
+                }
+            },
+            tower2: {
+                id: "tower2",
+                name: "Tower 2",
+                height: 234,
+                equipments: {
+                    equipment3: {
+                        id: "equipment3",
+                        name: "Equipment 3",
+                        brand: "ABC",
+                        price: 543
+                    },
+                    equipment4: {
+                        id: "equipment4",
+                        name: "Equipment 4",
+                        brand: "DSA",
+                        price: 654
+                    }
+                }
+            }
+        }
+    },
+    site2: {
+        id: "site2",
+        name: "Site 2",
+        description: "Description 2",
+        towers: {
+            tower3: {
+                id: "tower3",
+                name: "Tower 3",
+                height: 135,
+                equipments: {
+                    equipment5: {
+                        id: "equipment5",
+                        name: "Equipment 5",
+                        brand: "ABB 5",
+                        price: 321
+                    },
+                    equipment6: {
+                        id: "equipment6",
+                        name: "Equipment 6",
+                        brand: "Acme 6",
+                        price: 432
+                    }
+                }
+            },
+            tower4: {
+                id: "tower4",
+                name: "Tower 4",
+                height: 234,
+                equipments: {
+                    equipment7: {
+                        id: "equipment7",
+                        name: "Equipment 7",
+                        brand: "ABC 7",
+                        price: 543
+                    },
+                    equipment8: {
+                        id: "equipment8",
+                        name: "Equipment 8",
+                        brand: "DSA",
+                        price: 654
+                    }
+                }
+            }
+        }
+    },
+};
