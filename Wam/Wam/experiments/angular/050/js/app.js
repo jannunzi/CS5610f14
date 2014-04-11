d3df31afb0f03888f5a13be181bc9c90a31936fc
jannunzi <jannunzi@gmail.com>
2014-04-10 20:53:47 -0400

    Angular deep linking

---
+++ Wam/Wam/experiments/angular/050/js/app.js
@@ -0,0 +1,50 @@
+﻿var sampleApp = angular.module('sampleApp', ['ngRoute']);
+
+sampleApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/Page1', {
+            templateUrl: 'partial/page1.html',
+            controller: 'Page1Controller'
+        }).
+        when('/Page2', {
+            templateUrl: 'partial/page2.html',
+            controller: 'Page2Controller'
+        }).
+        when('/Page3', {
+            templateUrl: 'partial/page3.html',
+            controller: 'Page3Controller'
+        }).
+        when('/Page4', {
+            templateUrl: 'partial/page4.html',
+            controller: 'Page4Controller'
+        }).
+        otherwise({
+            redirectTo: '/Page1'
+        });
+  }]);
+
+
+sampleApp.controller('Page1Controller', function ($scope) {
+
+    $scope.message = 'This is Page 1';
+
+});
+
+sampleApp.controller('Page2Controller', function ($scope) {
+
+    $scope.message = 'This is Page 2';
+
+});
+
+sampleApp.controller('Page3Controller', function ($scope) {
+
+    $scope.message = 'This is Page 3';
+
+});
+
+sampleApp.controller('Page4Controller', function ($scope) {
+
+    $scope.message = 'This is Page 4';
+
+});
