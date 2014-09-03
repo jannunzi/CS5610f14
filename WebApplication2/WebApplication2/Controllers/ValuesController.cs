b989163229156f932498a0269c3be5cbc0691475
jannunzi <jannunzi@gmail.com>
2014-09-03 11:03:44 -0400

    Latest

---
+++ WebApplication2/WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public List<FishTO> Get()
+        {
+            List<FishTO> fs = new List<FishTO>();
+//            /*
+            using (f360Entities model = new f360Entities())
+            {
+                var fishes = from f in model.Fish select f;
+                foreach(Fish ff in fishes) {
+                    fs.Add(new FishTO(ff));
+                }
+            }
+  //          */
+            return fs;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public List<FishTO> Post([FromBody]FishTO fishTO)
+        {
+            List<FishTO> fs = new List<FishTO>();
+            Fish fish = fishTO.toFish();
+            using (f360Entities model = new f360Entities())
+            {
+                model.Fish.Add(fish);
+                model.SaveChanges();
+
+                var fishes = from f in model.Fish select f;
+                foreach (Fish ff in fishes)
+                {
+                    fs.Add(new FishTO(ff));
+                }
+            }
+            return fs;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/FishTO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class FishTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double weight { get; set; }
+        public double length { get; set; }
+        public string species { get; set; }
+        public System.DateTime caughtDate { get; set; }
+        public int tripId { get; set; }
+        public string notes { get; set; }
+        public System.TimeSpan caughtTime { get; set; }
+        public double girth { get; set; }
+        public string spot { get; set; }
+        public string presentation { get; set; }
+        public double depth { get; set; }
+        public double clarity { get; set; }
+        public double temperature { get; set; }
+
+        public Fish toFish()
+        {
+            Fish fish = new Fish
+            {
+                id = this.id,
+                name = this.name,
+                weight = this.weight,
+                length = this.length,
+                species = this.species,
+                caughtDate = this.caughtDate,
+                tripId = this.tripId,
+                notes = this.notes,
+                caughtTime = this.caughtTime,
+                girth = this.girth,
+                spot = this.spot,
+                presentation = this.presentation,
+                depth = this.depth,
+                clarity = this.clarity,
+                temperature = this.temperature
+            };
+            return fish;
+        }
+
+        public FishTO() { }
+
+        public FishTO(Fish other)
+        {
+            this.name = other.name;
+            this.id = other.id;
+            if (other.weight != null)
+                this.weight = other.weight.Value;
+            if (other.length != null)
+                this.length = other.length.Value;
+            this.species = other.species;
+            if (other.caughtDate != null)
+                this.caughtDate = other.caughtDate.Value;
+            if (other.tripId != null)
+                this.tripId = other.tripId.Value;
+            this.notes = other.notes;
+            if (other.caughtTime != null)
+                this.caughtTime = other.caughtTime.Value;
+            if (other.girth != null)
+                this.girth = other.girth.Value;
+            this.spot = other.spot;
+            this.presentation = other.presentation;
+            if (other.depth != null)
+                this.depth = other.depth.Value;
+            if (other.clarity != null)
+                this.clarity = other.clarity.Value;
+            if (other.temperature != null)
+                this.temperature = other.temperature.Value;
+        }
+    }
+}
